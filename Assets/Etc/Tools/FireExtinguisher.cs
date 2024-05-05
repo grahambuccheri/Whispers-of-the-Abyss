b25a5d10f997c694e285022991ac6086ba9f3ec8
alextvan <alextvan@yahoo.com>
2024-05-05 17:36:06 -0400

    FireExtinguisher Mechanic
    Hopefully works

--- Assets/Etc/Tools/FireExtinguisher.cs
+++ Assets/Etc/Tools/FireExtinguisher.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,8 @@ public class FireExtinguisher : MonoBehaviour, IInteractableTool
     [SerializeField] private ParticleSystem fireExtinguisher;
     [SerializeField] private float fireExtinguisherJuice;
     [SerializeField] private float fireExtinguisherDecreaseRate;
+
+    [SerializeField] DamageControl damageControl;
 
     [SerializeField] Camera mainCamera;
 
@@ -22,15 +25,20 @@ public class FireExtinguisher : MonoBehaviour, IInteractableTool
     {
         if (fireExtinguisherJuice > 0)
         {
-            fireExtinguisher.Play();
+            fireExtinguisher.Play(); // Play particle
+
+            // Decrease juice
             fireExtinguisherJuice -= fireExtinguisherDecreaseRate * Time.deltaTime;
 
+            // Create Ray
             Ray r = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
-            Debug.DrawLine(r.origin, r.direction);
+            // Debug.DrawLine(r.origin, r.direction);
 
+            // Send a raycast to a game object
             if (Physics.Raycast(r, out RaycastHit hitInfo, data.interactDistance))
             {
                 // Debug.Log(hitInfo.collider.gameObject.name);
+                increaseHealth(hitInfo);
             }
         }
         else
@@ -43,5 +51,31 @@ public class FireExtinguisher : MonoBehaviour, IInteractableTool
     public void stopInteract()
     {
         fireExtinguisher.Stop();
+    }
+
+    // This method matches the raycast hit info with whatever game object it is shooting at.
+
+    // Specifically, we want to focus on the battery, reactor, motor, and control panel.
+    public void increaseHealth(RaycastHit hitInfo)
+    {
+        string gameObjectName = hitInfo.collider.gameObject.name;
+        Debug.Log(gameObjectName);
+
+        if (gameObjectName.Equals(damageControl.batteryObject.name))
+        {
+            damageControl.batteryHealth += fireExtinguisherDecreaseRate * Time.deltaTime;
+        }
+        else if (gameObjectName.Equals(damageControl.reactorObject.name))
+        {
+            damageControl.reactorHealth += fireExtinguisherDecreaseRate * Time.deltaTime;
+        }
+        else if (gameObjectName.Equals(damageControl.motorObject.name))
+        {
+            damageControl.motorHealth += fireExtinguisherDecreaseRate * Time.deltaTime;
+        }
+        else if (gameObjectName.Equals(damageControl.displayObject.name))
+        {
+            damageControl.displayHealth += fireExtinguisherDecreaseRate * Time.deltaTime;
+        }
     }
 }
