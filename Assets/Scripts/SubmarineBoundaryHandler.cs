6cb87171a0a3150d641e99dd2b55f70ac9651fc9
the-urge <72226986+the-urge@users.noreply.github.com>
2024-05-06 04:00:25 -0400

    merge

--- Assets/Scripts/SubmarineBoundaryHandler.cs
+++ Assets/Scripts/SubmarineBoundaryHandler.cs
@@ -29,4 +29,9 @@ public class SubmarineBoundaryHandler : MonoBehaviour
             // TODO warn close to boundary!
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // TODO
+    }
 }
